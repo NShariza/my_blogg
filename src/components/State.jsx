7d2e7317e5c9cb092accc1ad6020ae8448e73b96
NShariza <sharizalynn@gmail.com>
2022-07-25 21:11:39 +0600

    feat: added info in jsx files

---
+++ src/components/State.jsx
@@ -0,0 +1,16 @@
+/*
+import React, {useState} from "react";
+
+const State = () => {
+    const [text, setText] = useState('No click');
+
+    return (
+      <div className="container">
+        <button onClick={() => setText('Click')}>click me</button>
+        <p>{text}</p>
+      </div>
+  )
+}
+
+export default State;
+*/

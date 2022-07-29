c88f797f73c377548bacaafa379ec86b5f096799
NShariza <sharizalynn@gmail.com>
2022-07-29 10:24:07 +0600

    feat: 29-07

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import Header from './components/Header/Header';
 import Main from './pages/Main/Main';
 import About from './pages/About/About';
 import Contacts from './pages/Contacts/Contacts';
-import Adress from './pages/Adress/Adress';
-import Vacancies from './pages/Vacancies/Vacancies';
-import Support from './pages/Support/Support';
 import NotFound from './pages/NotFound/NotFound';
 import Catalog from './pages/Catalog/Catalog';
 import Iphone from './pages/Iphone/Iphone';
@@ -16,8 +13,8 @@ import MacBook from './pages/MacBook/MacBook';
 function App() {
   return (
     <>
-      <Header/>
       <BrowserRouter>
+        <Header/>
         <Routes>
           <Route path="/" element={<Main pageName="Main Page"/>}/>
           <Route path="/about" element={<About text="Here is the info about us"/>}/>
@@ -26,9 +23,6 @@ function App() {
             <Route path="iphone" element={<Iphone/>}/>
             <Route path="macBook" element={<MacBook/>}/>
           </Route>
-          <Route path="/adress" element={<Adress adress="Here's my location"/>}/>
-          <Route path="/vacancies" element={<Vacancies work="Wanna join us?"/>}/>
-          <Route path="/support" element={<Support sup="You need some help babe?"/>}/>
           <Route path="*" element={<NotFound/>}/>
         </Routes>
       </BrowserRouter>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,22 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import styles from './Header.module.css';
 
 const Header = () => {
-    return (
-        <div className={styles.header}>
-            <ul>
-                <li><a href="/">Main</a></li>
-                <li><a href="/about">About us</a></li>
-                <li><a href="/contacts">Contacts</a></li>
-                <li><a href="/adress">Adress</a></li>
-                <li><a href="/vacancies">Vacancies</a></li>
-                <li><a href="/support">Support</a></li>
-                <li><a href="/iphone">Iphone</a></li>
-            </ul>
-        </div>
-    );
+
+  const setActive = ({isActive}) => isActive ? styles.active : "";
+  return (
+    <div className={styles.header}>
+      <ul>
+        <li><NavLink className={setActive} to="/">Main</NavLink></li>
+        <li><NavLink className={setActive} to="/about">About us</NavLink></li>
+        <li><NavLink className={setActive} to="/contacts">Contacts</NavLink></li>
+        <li><NavLink className={setActive} to="/catalog">Catalog</NavLink></li>
+        <li><NavLink className={setActive} to="/catalog/iphone">Iphone</NavLink></li>
+        <li><NavLink className={setActive} to="/catalog/macBook">MacBook</NavLink></li>
+      </ul>
+    </div>
+  );
 }
 
 export default Header;
